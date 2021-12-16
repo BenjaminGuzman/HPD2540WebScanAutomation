c478a37a2e156726d8840237f8e6ffeaf7271696
BenjaminGuzman <9benjaminguzman@gmail.com>
2021-12-15 22:42:14 -0600

    Repo started

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # HPD2540WebScanAutomation
 Utility script to automate scanning with an HP Deskjet Ink Advantage 2540 All-in-One Printer
+
+[scan-interval.js](scan-interval.js) is an script to add some util buttons to the printer UI to reduce the number of times you have to press the scan button.
+
+Before applying the script
+![Before](before.png)
+
+After applying the script
+![After](after.png)
+
+The only thing you have to do is copy and paste the script in the browser dev tools.
+
+## TODO's
+
+- It'd be nice if the download (Save Image As...) process is also automated.
+
+IDK how to do this with JS, I don't think it is possible or may require much more code and possibly reverse-engineer the web app.
---
+++ scan-interval.js
@@ -0,0 +1,56 @@
+const scanBtn = document.getElementsByClassName("gui-cmd-btn-scan gui-action-btn")[0];
+const buttonsDiv = document.getElementById("app-page-btns");
+
+let intervalId = 0; // id of the interval that will press start button every n seconds
+let interval = 0; // number of (minimum) millis to wait until the next scan
+
+/**
+ * Call this to start an interval to click the scan button every {@code interval} seconds
+ */
+const start = () => {
+	if (intervalId) { // if it is a truthy, the interval may still be running. Don't start a new interval
+		alert("Interval is running. Stop it first");
+		return;
+	}
+	intervalId = setInterval(() => scanBtn.click(), interval);
+	console.log(`${interval / 1_000}s interval has started. Interval id: ${intervalId}`);
+};
+
+/**
+ * Call this to stop the interval initiated by its counterpart start
+ */
+const stop = () => {
+	if (!intervalId) // if falsy, there is no interval running
+		return;
+	clearInterval(intervalId);
+	intervalId = 0;
+};
+
+// create range input to allow user select the interval
+const template = document.createElement("template");
+template.innerHTML = `<div style="display: flex; align-items: center; gap: 0.5rem">
+	<input type="button" id="stop-interval" class="gui-action-btn" value="Stop interval" style="width: 100px; text-align: center;">
+	<input type="range" id="scan-interval" min="5" max="60" value="10" step="1">
+	<span id="scan-interval-indicator">10 s</span>
+</div>`.trim();
+const intervalInput = template.content.getElementById("scan-interval");
+const intervalIndicator = template.content.getElementById("scan-interval-indicator");
+const stopIntervalBtn = template.content.getElementById("stop-interval");
+intervalInput.addEventListener("input", () => { // when the input changes update the ui and interval value
+	interval = parseInt(intervalInput.value) * 1_000; // the interval value should be in milliseconds
+	intervalIndicator.innerText = `${intervalInput.value} s`;
+});
+stopIntervalBtn.onclick = (e) => {
+	stop();
+	e.preventDefault();
+}
+
+// start the interval as soon the user presses scan for the first time
+let firstTime = true;
+scanBtn.addEventListener("click", () => {
+	if (firstTime)
+		start();
+	firstTime = false;
+});
+
+buttonsDiv.getElementsByClassName("btn-list")[0].appendChild(template.content);
